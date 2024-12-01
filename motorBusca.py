fcb7f19b176c408fbf49ca9cded1b523b4f6f5b0
Renef <renefricardo@gmail.com>
2024-12-01 01:10:04 -0300

    upd funcao de buscar

--- motorBusca.py
+++ motorBusca.py
@@ -1,23 +1,27 @@
 from lxml import etree as et
 import re
 from fuzzywuzzy import fuzz
-#print(f'Num de páginas: {len(paginas)}')
+
+
+# print(f'Num de páginas: {len(paginas)}')
 # for pagina in paginas:
 #     paginaId = pagina.find('id').text
-#     paginaTitulo = pagina.find('title').text
-#     print(f'Id: {paginaId}: Titulo: {paginaTitulo}\n')
+#     pagina_titulo = pagina.find('title').text
+#     print(f'Id: {paginaId}: Titulo: {pagina_titulo}\n')
 
 
 def abrirXML():
     arquivo = et.parse('verbetesWikipedia.xml')
     raiz = arquivo.getroot()
     paginas = raiz.xpath('//page')
-    return  paginas
+    return paginas
 
-def buscaPalavrainteira(termo, texto):
-    #busca a palavra inteira ignorando especias
+
+def buscaPalavraInteira(termo, texto):
+    # busca a palavra inteira ignorando especias
     palavra = r'\b' + re.escape(termo) + r'\b'
     return bool(re.search(palavra, texto, re.IGNORECASE))
+
 
 def buscaPartePalavra(termo, texto):
     # busca palavras 80% similares
@@ -26,32 +30,60 @@ def buscaPartePalavra(termo, texto):
         return True
 
 
-def buscarTitulo(termo_buscado):
+def buscarTermo(termo_buscado):
     termo_buscado = termo_buscado.lower()
     paginas = abrirXML()
-    titulos_encontrados = {}
+    artigos_encontrados = {}
 
     for pagina in paginas:
-        paginaTitulo = pagina.find('title').text
-        for palavra in paginaTitulo.split():
-            if buscaPalavrainteira(termo_buscado, palavra) or buscaPartePalavra(termo_buscado, palavra):
-                if paginaTitulo not in titulos_encontrados:
-                    titulos_encontrados[paginaTitulo] = pagina.find('id').text
-                break
+        pagina_titulo = pagina.find('title').text
+        pagina_texto = pagina.find('text').text
+        for palavra in pagina_texto.split():
+            if buscaPalavraInteira(termo_buscado, palavra):  # or buscaPartePalavra(termo_buscado, palavra):
+                if pagina_titulo not in artigos_encontrados:
+                    artigos_encontrados[pagina_titulo] = (pagina.find('id').text, pagina.find('text').text)
+                    break
+    return artigos_encontrados
 
-    return titulos_encontrados
+
+def relevancia(artigos, termo_buscado):
+    artigos_classificados = {}
+    for artigo_titulo, (artigo_id, artigo_texto) in artigos.items():
+        relevancia = 0
+        num_palavras = 0
+        num_correspondecias = 0
+
+        for palavra in artigo_texto.split():
+            num_palavras += 1
+            if buscaPalavraInteira(termo_buscado, palavra):
+                num_correspondecias += 1
+
+        if num_palavras > 0:
+            relevancia = num_correspondecias / num_palavras
+
+        if buscaPalavraInteira(termo_buscado, artigo_titulo):
+            relevancia += 0.1
+
+        artigos_classificados[artigo_id] = (artigo_titulo, relevancia)
+
+    return artigos_classificados
 
 
+def buscar(termo_buscado):
+    termo_buscado = termo_buscado.lower()
+    artigos_encontrados = buscarTermo(termo_buscado)
+    artigos_classificados = relevancia(artigos_encontrados, termo_buscado)
 
-# TODO: implementacao da busca
-def busca():
-    pass
+    artigos_ordenados = sorted(artigos_classificados.items(), key=lambda x: x[1][1], reverse=True)
 
-resultados = buscarTitulo('computers')
+    return artigos_ordenados
+
+resultados = buscar('computers')
 
 cont = 0
-for titulo, id in resultados.items():
-    print(f'Id {id}, Titulo {titulo} \n')
-    cont += 1
 
-print('Resultados encontrados',cont)
+for artigo_id, (artigo_titulo, relevancia) in resultados[:5]:
+    print(f'Id: {artigo_id},Titulo: {artigo_titulo}, Relevancia: {(relevancia*10):.2f}\n')
+    print('-'*40)
+    cont += 1
+print('Resultados encontrados: ', cont)
